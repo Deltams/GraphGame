9088141a46a57e4a139150ea5490d07d19692139
Дмитрий <83587062+Deltams@users.noreply.github.com>
2022-05-19 01:59:30 +0800

    Update desktop_task2_V_1_0.py

--- task2/desktop_task2_V_1_0.py
+++ task2/desktop_task2_V_1_0.py
@@ -1,4 +1,4 @@
-from tkinter import *
+Ffrom tkinter import *
 import random
 import tkinter as tk
 from PIL import Image, ImageTk
@@ -692,7 +692,7 @@ def input_mi():
     top_canvas = Canvas(root, width=1100, height=70, bg='#fad7d4')
     top_canvas.pack()
 
-    label_name_ss_ms_mi = Label(text='Матрица ицидентности', font=('Arial', 14, 'normal'))
+    label_name_ss_ms_mi = Label(text='Матрица инцидентности', font=('Arial', 14, 'normal'))
     label_name_ss_ms_mi_win = top_canvas.create_window(518, 20, window=label_name_ss_ms_mi)
     
 ##    label_v = Label(text='Количество вершин', font=('Arial', 12, 'normal'))
